491836cdd9bab857001894601c5f7f2f6b66efa9
g-akca <gok.akca66@gmail.com>
2026-06-21 20:24:58 +0300

    Update index view to display message when no categories are found

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -2,12 +2,6 @@ import db from "../db/queries.js";
 
 async function getAllCategories(req, res) {
   const categories = await db.getAllCategories();
-
-  if (!categories) {
-    res.status(404).send("No categories were found");
-    return;
-  }
-
   return categories;
 }
 
