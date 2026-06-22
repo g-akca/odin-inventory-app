4ea2d8b92778001d586bc1aec48f490fba5c4412
g-akca <gok.akca66@gmail.com>
2026-06-22 13:07:44 +0300

    Add insertCategory and insertItem functions to queries

--- db/queries.js
+++ db/queries.js
@@ -1,5 +1,6 @@
 import pool from "./pool.js";
 
+// SELECT queries
 async function getAllCategories() {
   const { rows } = await pool.query("SELECT * FROM categories");
   return rows;
@@ -18,6 +19,15 @@ async function getItemsByCategory(categoryId) {
 async function getItemById(itemId) {
   const { rows } = await pool.query("SELECT * FROM items WHERE id = $1", [itemId]);
   return rows[0];
+}
+
+// INSERT queries
+async function insertCategory(name) {
+  await pool.query("INSERT INTO categories VALUES ($1)", [name]);
+}
+
+async function insertItem(name, categoryId, quantity, unit, price) {
+  await pool.query("INSERT INTO items VALUES ($1, $2, $3, $4, $5)", [name, categoryId, quantity, unit, price]);
 }
 
 export default { getAllCategories, getCategoryById, getItemsByCategory, getItemById };
